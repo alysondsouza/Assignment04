7d4e70cdf760c5a19c33ffd688ed0b59195a2fbb
Alyson De Souza <alysondsouza@yahoo.com>
2021-10-01 13:00:44 +0200

    postgres connector

--- Assignment4-main/Assignment4/Program.cs
+++ Assignment4-main/Assignment4/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+//CMD: TO CREATE EF CORE
+//dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+//CMD: TO BIND DB POSTGRES
+//dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL //--version 5.0.10
+
 namespace Assignment4
 {
     class Program
